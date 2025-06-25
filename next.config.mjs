86c479d194fb3b3838e1471f5e58ed0bda1c0edb
Duy-Nam Ly <51230585+lyduynam@users.noreply.github.com>
2025-06-25 14:29:04 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -14,15 +14,7 @@ const nextConfig = {
      */
     basePath: "/",
   
-    /**
-     * Disable server-based image optimization. Next.js does not support
-     * dynamic features with static exports.
-     *
-     * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
-     */
-    images: {
-      unoptimized: true,
-    },
+    
   };
 
 export default nextConfig;
