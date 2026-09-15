5b2b8c5b896c81f6a4e5a25b742f00da6c019699
unknown <duynamvt79@gmail.com>
2026-09-15 15:14:05 +0800

    fix tailwindcss

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    /**
-     * Enable static exports.
-     *
-     * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-     */
+    reactStrictMode: true, 
     output: "export",
-  
-    /**
-     * Set base path. This is the slug of your GitHub repository.
-     *
-     * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-     */
-    basePath: "/",
-  
-    
-  };
 
+
+    basePath: "/",
+};
 export default nextConfig;
